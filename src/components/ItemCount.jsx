c8e29047ef1de6ffb02bffd0e1c53b31edc5a599
Leo Nania <leo.nania18@gmail.com>
2022-04-04 10:25:11 -0300

    Agregando funcionalidad a botones de la card

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,16 +1,35 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Card, Button } from 'react-bootstrap';
 import producto1 from '../imagenes/producto1.jpeg';
 
 
 
 
-const ItemCount = () => {
-    const [ItemsCantidad, setItemsCantidad] = useState(0);
+const ItemCount = ({stock = 0 ,initial = 1, onAdd}) => {
+    const [count, setCount] = useState(0);
+
+  useEffect(()=>{
+    setCount(initial);
+  },[]);
+
+  const increment = () =>{
+    if (count < stock) {
+      setCount(count + 1);
+    }
+  }
+
+  const decrement = () =>{
+    if (count > initial){
+      setCount(count - 1);
+    }
+  }
+
+
+    
     return (
-        <div>
+        <div className="cardEntera">
           <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={producto1} />
+  <Card.Img variant="top" src={producto1}/>
   <Card.Body>
     <Card.Title>Bombilla de acero</Card.Title>
     <Card.Text>
@@ -18,9 +37,16 @@ const ItemCount = () => {
     </Card.Text>
 
 
-   <Button variant="primary">-</Button><h1></h1> 
-    <span>{ItemsCantidad}</span>
-   <Button variant="primary">+</Button>
+    <Button variant="outline-info" className="buttonCard" onClick={increment}> - </Button> 
+     <span style={{ fontSize: "1.5rem", color: "black"}} className="buttonCard">{count}</span> 
+     <Button variant="outline-info" className="buttonCard"onClick={decrement}>+</Button> <br></br>
+
+
+               {
+            stock
+                ? <Button variant="contained" color="primary" onClick={() => onAdd(count)}>Add to Cart</Button>
+                : <Button variant="contained" disabled>Add to Cart</Button>
+                }
 
 
   </Card.Body>
@@ -30,3 +56,13 @@ const ItemCount = () => {
 }
 
 export default ItemCount;
+
+/* 
+<div >
+    <Button onClick={()=> setItemsCantidad (ItemsCantidad - 1)} variant="outline-info" className="buttonCard"> - </Button> 
+     <span style={{ fontSize: "1.5rem", color: "black"}} className="buttonCard">{ItemsCantidad}</span> 
+     <Button onClick={()=> setItemsCantidad (ItemsCantidad + 1)} variant="outline-info" className="buttonCard">+</Button> <br></br>
+
+</div>
+    <Button variant="primary" className="addCart">Añadir al carrito</Button>
+*/
